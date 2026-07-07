d0ff3c972251b2e85f2d32695c3f7b6b357e7645
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2026-07-07 10:42:34 -0300

    providers, variables

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "image_id" {
+    default = "ami-0a15226b1f7f23580"
+    type = string
+    description = "The id of het machine image (AMI) to use for the server."
+
+    validation {
+      condition = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
+      error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
+    }
+}
